1d30ef902eba48f672b6ec75b28ee00f5ed44f08
DinoBTW3 <dinobtw3@gmail.com>
2025-02-09 13:54:35 +0100

    init

---
+++ feller.lua
@@ -0,0 +1,9 @@
+local has_block, data = turtle.inspect()
+if has_block then
+    repeat
+        turtle.dig(south)
+        turtle.digUp()
+        has_block, data = turtle.inspect()  -- Re-check the block after each operation
+    until data == false
+    turtle.up(1)
+end
