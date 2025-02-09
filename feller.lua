6e7c29586094adb72d1d7ef2e739355c2cae507d
DinoBTW3 <dinobtw3@gmail.com>
2025-02-09 15:00:55 +0100

    update4

--- feller.lua
+++ feller.lua
@@ -1,10 +1,22 @@
+function printTable(tbl, indent)
+    indent = indent or 0
+    local prefix = string.rep("  ", indent)
+
+    for key, value in pairs(tbl) do
+        if type(value) == "table" then
+            print(prefix .. key .. ":")
+            printTable(value, indent + 1) -- Recursively print nested tables
+        else
+            print(prefix .. key .. ": " .. tostring(value))
+        end
+    end
+end
+
 function blockInfrontIsLog()
     local has_block, data = turtle.inspect()
 
     if has_block then
-        for key, value in pairs(data) do
-            print(key .. ": " .. tostring(value))
-        end
+        printTable(data)
     end
 end
 
