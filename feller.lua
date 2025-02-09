273616805299040d5cf586c282015e3cd0592ef4
DinoBTW3 <dinobtw3@gmail.com>
2025-02-09 15:12:55 +0100

    Update6

--- feller.lua
+++ feller.lua
@@ -2,12 +2,8 @@ function blockInfrontIsLog()
     local has_block, data = turtle.inspect()
 
     if has_block then
-        for key, value in pairs(data) do
-            if key == "name" then
-                if string.find(tostring(value), "log") then
-                    return true
-                end
-            end
+        if data.name and string.find(data.name, "log") then
+            return true
         end
     end
     
@@ -20,19 +16,24 @@ function refuel()
     
     local ok, err = turtle.refuel()
     if ok then
-      local new_level = turtle.getFuelLevel()
-      print(("Refuelled %d, current level is %d"):format(new_level - level, new_level))
+        local new_level = turtle.getFuelLevel()
+        print(("Refueled %d, current level is %d"):format(new_level - level, new_level))
     else
-      printError(err)
+        printError(err)
     end
 end 
 
 print(blockInfrontIsLog())
 
-while blockInfrontIsLog do
-    refuel()
-    turtle.dig(south)
+while blockInfrontIsLog() do
+    --refuel()
+    turtle.dig() -- Dig forward
     turtle.up()
-    turtle.sleep(1)
+    sleep(1) -- Sleep for 1 second
     turtle.digUp()
+end
+
+-- When no more logs are in front, move down until the turtle reaches the ground
+while not turtle.detectDown() do
+    turtle.down()
 end
